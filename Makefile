c32242655a66a57f23ba9b322faf3f41ae327a81
Martin Steffen <msteffen@ifi.uio.no>
2023-01-11 10:03:09 +0100

    --

---
+++ Makefile
@@ -0,0 +1,17 @@
+## copy to the local web server.
+bserve:
+	bundle exec jekyll serve -d   ./../../localwebpage/
+
+#lexport:
+#	jekyll build -d   ./../localwebpage/
+
+
+realexport:
+	bundle exec jekyll build -d   ~/cor/teaching/2040-test/IN2040-test.pages.github.uio.no/
+
+
+testrealexport:
+	bundle exec jekyll build -d     ~/cor/teaching/in2040test.github.io/
+
+
+
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Welcome to Functional programming (IN2040), autumn 2022",
+        "excerpt":"About this semester After a couple of corona semesters, the forthcoming one seems to be unaffected by any viruses, and things are back to normal. So right now, at the end of the spring semester, we plan for a semester in a standard, non-corona set-up. We should, however, keep in...","categories": ["functionalprogramming"],
+        "tags": ["functional programming","IN2040","scheme","lisp"],
+        "url": "/functionalprogramming/2022/06/17/fpwelcome-2022.html",
+        "teaser": null
+      },{
+        "title": "Recursion, primitive or otherwise",
+        "excerpt":"The SICP textbook shows in Exercise 1.10 a famous function known as Ackermann’s function. Actually the code there shows one version of that function; there are minor variations of it, all doing basically the same, and all known as Ackermann function. The exercise is not on the list of exercises...","categories": ["functionalprogramming"],
+        "tags": ["functional programming","IN2040","scheme","lisp","Ackermann","currying","higher-order functions","recursion","primitive recursion"],
+        "url": "/functionalprogramming/2022/07/07/ackermann.html",
+        "teaser": null
+      },{
+        "title": "Y Y?",
+        "excerpt":"This is another post in connection with some slide showed in the lecture, which may have been a bit obscur. As with all these posts, the background information here is non-pensum. It’s triggered by some slide week 09 about```recursion with anonymous procedures’’. The slide showed a version of faculty programmed...","categories": ["functionalprogramming"],
+        "tags": ["Y combinator","IN2040","lambda calculus","recursion","Turing complete","computable functions","foundations","Church numerals"],
+        "url": "/functionalprogramming/2022/10/13/ycombinator.html",
+        "teaser": null
+      },{
+        "title": "Evaluation strategies",
+        "excerpt":"The post is about evaluation strategies. The concept was discussed in the lecture (in week 2) and in SICP. Epecially applicative order evaluation is covered, as the standard evaluation strategy of scheme. Also, an alternative to that is discussed, namely normal order evaluation, and that’s done in connection with things...","categories": ["functionalprogramming"],
+        "tags": ["functional programming","IN2040","scheme","lisp"],
+        "url": "/functionalprogramming/2022/12/21/evaluationstrategies.html",
+        "teaser": null
+      }]
