04bd855200e60a29dcbf8c4358a0dd8d12e994dc
Martin Steffen <msteffen@ifi.uio.no>
2023-08-24 12:56:01 +0200

    --

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,27 +5,9 @@ var store = [{
         "url": "/functionalprogramming/2022/06/17/fpwelcome-2022.html",
         "teaser": null
       },{
-        "title": "Recursion, primitive or otherwise",
-        "excerpt":"The SICP textbook shows in Exercise 1.10 a famous function known as Ackermann’s function. Actually the code there shows one version of that function; there are minor variations of it, all doing basically the same, and all known as Ackermann function. The exercise is not on the list of exercises...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","scheme","lisp","Ackermann","currying","higher-order functions","recursion","primitive recursion"],
-        "url": "/functionalprogramming/2022/07/07/ackermann.html",
-        "teaser": null
-      },{
-        "title": "Evaluation strategies",
-        "excerpt":"The post is about evaluation strategies. The concept was discussed in the lecture (in week 2) and in SICP. Epecially applicative order evaluation is covered, as the standard evaluation strategy of scheme. Also, an alternative to that is discussed, namely normal order evaluation, and that’s done in connection with things...","categories": ["functionalprogramming"],
+        "title": "Welcome to Functional programming (IN2040), autumn 2023",
+        "excerpt":"About this semester Lectures Corona is over (and let’s hope it won’t come back any time soon). So lectures, will be held the way it used to be, in the lecture hall (Simula@OJD). youtube One perhaps positive effect of the virus times was that many lectures produced video-ed versions of...","categories": ["functionalprogramming"],
         "tags": ["functional programming","IN2040","scheme","lisp"],
-        "url": "/functionalprogramming/2022/12/21/evaluationstrategies.html",
-        "teaser": null
-      },{
-        "title": "Y Y?",
-        "excerpt":"This is another post in connection with some slides shown in the lecture, which may have been a bit obscure. The text here is concretely triggered by a slide in week 9 about “recursion with anonymous procedures”. The slide showed a version of the factorial function programmed in a way...","categories": ["functionalprogramming"],
-        "tags": ["Y combinator","lambda calculus","recursion","Turing completeness","computable functions","foundation","Church numerals"],
-        "url": "/functionalprogramming/2023/01/21/ycombinator.html",
-        "teaser": null
-      },{
-        "title": "Y not code up $n!$ with no recursion and no Y tricks either?",
-        "excerpt":"In another post, I dissected how to program recursion using anoymous functions, only. This ultimately leads to the famous $Y$-combinator. Actually, there are multiple versions of it, all doing the recursion-trick by some form of self-application. The running example in that blog post was the inevitable factorial function. If that...","categories": ["functionalprogramming"],
-        "tags": ["Church numerals","lambda calculus","induction","for loops","recursion","foundations","pattern matching","inductive data types"],
-        "url": "/functionalprogramming/2023/02/07/churchnums.html",
+        "url": "/functionalprogramming/2023/08/18/fpwelcome-2023.html",
         "teaser": null
       }]
