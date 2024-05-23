ab655a564e8292cf80fd0076f351cee628be7f66
Martin Steffen <msteffen@ifi.uio.no>
2024-05-23 06:43:30 +0200

    up again

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,61 +1,7 @@
 var store = [{
-        "title": "Welcome to Functional programming (IN2040), autumn 2022",
-        "excerpt":"About this semester After a couple of corona semesters, the forthcoming one seems to be unaffected by any viruses, and things are back to normal. So right now, at the end of the spring semester, we plan for a semester in a standard, non-corona set-up. We should, however, keep in...","categories": ["functionalprogramming"],
+        "title": "Welcome to Functional programming (IN2040), autumn 2024",
+        "excerpt":"About this semester Lectures Corona is a thing of the dim pasts and lectures will be held the way it used to be, in the lecture hall (Simula@OJD). Youtube One perhaps positive effect of the virus times was that many lectures produced video-ed versions of the presentations, like screencasts or...","categories": ["functionalprogramming"],
         "tags": ["functional programming","IN2040","scheme","lisp"],
-        "url": "/functionalprogramming/2022/06/17/fpwelcome-2022.html",
-        "teaser": null
-      },{
-        "title": "Welcome to Functional programming (IN2040), autumn 2023",
-        "excerpt":"About this semester Lectures Corona is over (and let’s hope it won’t come back any time soon). So lectures, will be held the way it used to be, in the lecture hall (Simula@OJD). youtube One perhaps positive effect of the virus times was that many lectures produced video-ed versions of...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","scheme","lisp"],
-        "url": "/functionalprogramming/2023/08/18/fpwelcome-2023.html",
-        "teaser": null
-      },{
-        "title": "Evaluation strategies",
-        "excerpt":"The post is about evaluation strategies. The concept was discussed in the lecture (in week 2) and in SICP. Especially applicative order evaluation is covered, as the standard evaluation strategy of scheme. Also, an alternative to that is discussed, namely normal order evaluation, and that’s done in connection with things...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","scheme","lisp","evaluation strategy","normal-order evaluation strategy","applicative-order evaluation strategy","referential transparency"],
-        "url": "/functionalprogramming/2023/09/11/evaluationstrategies.html",
-        "teaser": null
-      },{
-        "title": "Recursion, primitive or otherwise",
-        "excerpt":"The SICP textbook shows in Exercise 1.10 a famous function known as Ackermann’s function. Actually the code there shows one version of that function; there are minor variations of it, all doing basically the same, and all known as Ackermann function. The exercise is not on the list of exercises...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","scheme","lisp","Ackermann","currying","higher-order functions","recursion","primitive recursion"],
-        "url": "/functionalprogramming/2023/09/13/ackermann.html",
-        "teaser": null
-      },{
-        "title": "Processes and procedures",
-        "excerpt":"From the group sessions I got feedback that the concepts of processes vs procedures and functions remained shady and a bit unclear (at least for some). Thus I write a few lines on that. I won’t repeat the examples and sentences from SICP; there is Section 1.2 (“Procedures and the...","categories": ["functionalprogramming"],
-        "tags": ["procedure","process","function"],
-        "url": "/functionalprogramming/2023/09/26/processprocedure.html",
-        "teaser": null
-      },{
-        "title": "Y Y?",
-        "excerpt":"This is another post in connection with some slides shown in the lecture, which may have been a bit obscure. The text here is concretely triggered by a slide in week 9 about “recursion with anonymous procedures”. The slide showed a version of the factorial function programmed in a way...","categories": ["functionalprogramming"],
-        "tags": ["Y combinator","lambda calculus","recursion","Turing completeness","computable functions","foundation","Church numerals"],
-        "url": "/functionalprogramming/2023/10/09/ycombinator.html",
-        "teaser": null
-      },{
-        "title": "Y not code up $n!$ with no recursion and no Y tricks either?",
-        "excerpt":"In another post, I dissected how to program recursion using anonymous functions, only. This ultimately leads to the famous $Y$-combinator. Actually, there are multiple versions of it, all doing the recursion-trick by some form of self-application. The running example in that blog post was the inevitable factorial function. If that...","categories": ["functionalprogramming"],
-        "tags": ["Church numerals","lambda calculus","induction","for loops","recursion","foundations","pattern matching","inductive data types"],
-        "url": "/functionalprogramming/2023/10/20/churchnums.html",
-        "teaser": null
-      },{
-        "title": "(Not so) abstract syntax trees",
-        "excerpt":"In weeks 11 and 12, we take a look at the meta-circular interpreter from Chapter 4 of SICP. There are different parts we discusses, the central one the mutually recursive procedure eval and apply (resp. mc-eval and mc-apply when we want to use the names as used in the slides)....","categories": ["functionalprogramming"],
-        "tags": ["scheme","meta-circular evaluator","syntax","abstract syntax"],
-        "url": "/functionalprogramming/2023/11/03/notsoast.html",
-        "teaser": null
-      },{
-        "title": "Implicit stream definitions",
-        "excerpt":"The post was triggered by working out some solution to an older exam question (from 2017), about streams. This is not a straight answer to that exam question, for that it’s much too long, but it touches on different issues that are connected to streams, evaluation strategies, and it sheds...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","Scheme","substitution and environment model","evaluation strategies"],
-        "url": "/functionalprogramming/2023/12/05/implicitstreams.html",
-        "teaser": null
-      },{
-        "title": "How to prepare for an oral exam?",
-        "excerpt":"The standard exam for the course on functional programming is written. Except for the times under corona, where it was a variation of a written exam, a “home exam”. However, the repeat exams are regularly held as oral ones. One reason is simply expedience. There are not too many candidates...","categories": ["functionalprogramming"],
-        "tags": ["exam preparation","oral exam","functional programming","SICP"],
-        "url": "/functionalprogramming/2024/01/11/howtopreparefororalfp.html",
+        "url": "/functionalprogramming/2024/05/22/fpwelcome-2024.html",
         "teaser": null
       }]
