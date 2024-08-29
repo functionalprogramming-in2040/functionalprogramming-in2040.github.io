a4e8b7efd9b3e76ba297bbb53f6feb68baead064
Martin Steffen <msteffen@ifi.uio.no>
2024-08-29 06:17:39 +0200

    --

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["functional programming","IN2040","scheme","lisp"],
         "url": "/functionalprogramming/2024/05/22/fpwelcome-2024.html",
         "teaser": null
+      },{
+        "title": "Evaluation strategies",
+        "excerpt":"The post is about evaluation strategies. The concept was discussed in the lecture (in week 2) and in SICP. Especially applicative order evaluation is covered, as the standard evaluation strategy of scheme. Also, an alternative to that is discussed, namely normal order evaluation, and that’s done in connection with things...","categories": ["functionalprogramming"],
+        "tags": ["functional programming","IN2040","scheme","lisp","evaluation strategy","normal-order evaluation strategy","applicative-order evaluation strategy","referential transparency"],
+        "url": "/functionalprogramming/2024/08/29/evaluationstrategies.html",
+        "teaser": null
       }]
