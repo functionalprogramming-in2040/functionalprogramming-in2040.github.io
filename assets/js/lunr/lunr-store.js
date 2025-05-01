52ccb7fac4c6a8d50a4f9a6cf1b6999d7d52707d
Martin Steffen <msteffen@ifi.uio.no>
2025-05-01 08:18:37 +0200

    --

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,49 +1,7 @@
 var store = [{
-        "title": "Welcome to Functional programming (IN2040), autumn 2024",
+        "title": "Welcome to Functional programming (IN2040), autumn 2025",
         "excerpt":"About this semester Lectures Corona is a thing of the dim pasts and lectures will be held the way it used to be, in the lecture hall (Simula@OJD). Youtube One perhaps positive effect of the virus times was that many lectures produced video-ed versions of the presentations, like screencasts or...","categories": ["functionalprogramming"],
         "tags": ["functional programming","IN2040","scheme","lisp"],
-        "url": "/functionalprogramming/2024/05/22/fpwelcome-2024.html",
-        "teaser": null
-      },{
-        "title": "Evaluation strategies",
-        "excerpt":"The post is about evaluation strategies. The concept is discussed in the lecture (in week 2) and in SICP. Especially applicative order evaluation is covered, as the standard evaluation strategy of Scheme. Also, an alternative to that is discussed, namely normal order evaluation, and that’s done in connection with things...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","scheme","lisp","evaluation strategy","normal-order evaluation strategy","applicative-order evaluation strategy","referential transparency"],
-        "url": "/functionalprogramming/2024/08/29/evaluationstrategies.html",
-        "teaser": null
-      },{
-        "title": "What's in a name?",
-        "excerpt":"In the break during the lecture in week 2, a question came up from the audience asking for more information and explanations in connection with some slide titled “Anecdote”. Discussed at (and around) that slide are formal parameters, free and bound variables, and the notion of scope. The slides state...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","scheme","lisp","variables","scope","variable capture","variable renaming","static scoping","dynamic scoping"],
-        "url": "/functionalprogramming/2024/09/01/bindingandscope.html",
-        "teaser": null
-      },{
-        "title": "Processes and procedures",
-        "excerpt":"Previous semester I got feedback that the concepts of processes vs procedures and functions remained shady and a bit unclear (at least for some). Thus I write a few lines on that. I won’t repeat the examples and sentences from SICP; there is Section 1.2 (“Procedures and the processes generate”)...","categories": ["functionalprogramming"],
-        "tags": ["procedure","process","function"],
-        "url": "/functionalprogramming/2024/09/09/processprocedure.html",
-        "teaser": null
-      },{
-        "title": "Recursion, primitive or otherwise",
-        "excerpt":"The SICP textbook shows in Exercise 1.10 a famous function known as Ackermann’s function. Actually the code there shows one version of that function; there are minor variations of it, all doing basically the same, and all known as Ackermann function. The exercise is not on the list of exercises...","categories": ["functionalprogramming"],
-        "tags": ["functional programming","IN2040","scheme","lisp","Ackermann","currying","higher-order functions","recursion","primitive recursion"],
-        "url": "/functionalprogramming/2024/09/12/ackermann.html",
-        "teaser": null
-      },{
-        "title": "Y Y?",
-        "excerpt":"This is another post in connection with some slides shown in the lecture, which may have been a bit obscure. The text here is concretely triggered by a slide in week 9 about “recursion with anonymous procedures”. The slide showed a version of the factorial function programmed in a way...","categories": ["functionalprogramming"],
-        "tags": ["Y combinator","lambda calculus","recursion","Turing completeness","computable functions","foundation","Church numerals"],
-        "url": "/functionalprogramming/2024/10/17/ycombinator.html",
-        "teaser": null
-      },{
-        "title": "How to prepare for an oral exam?",
-        "excerpt":"The standard exam for the course on functional programming is written. Except for the times under corona, where it was a variation of a written exam, a “home exam”. However, the repeat exams are regularly held as oral ones. One reason is simply expedience. There are not too many candidates...","categories": ["functionalprogramming"],
-        "tags": ["exam preparation","oral exam","functional programming","SICP"],
-        "url": "/functionalprogramming/2025/01/24/howtopreparefororal.html",
-        "teaser": null
-      },{
-        "title": "How to prepare for an oral exam?",
-        "excerpt":"The standard exam for the course on functional programming is written. Except for the times under corona, where it was a variation of a written exam, a “home exam”. However, the repeat exams are regularly held as oral ones. One reason is simply expedience. There are not too many candidates...","categories": ["functionalprogramming"],
-        "tags": ["exam preparation","oral exam","functional programming","SICP"],
-        "url": "/functionalprogramming/2025/01/25/howtopreparefororal.html",
+        "url": "/functionalprogramming/2025/01/01/fpwelcome-2025.html",
         "teaser": null
       }]
