b3c2cf659c501d92903ccea079847ada5e60d463
Martin Steffen <msteffen@ifi.uio.no>
2025-05-01 08:20:58 +0200

    --

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Welcome to Functional programming (IN2040), autumn 2025",
         "excerpt":"About this semester Lectures Corona is a thing of the dim pasts and lectures will be held the way it used to be, in the lecture hall (Simula@OJD). Youtube One perhaps positive effect of the virus times was that many lectures produced video-ed versions of the presentations, like screencasts or...","categories": ["functionalprogramming"],
         "tags": ["functional programming","IN2040","scheme","lisp"],
-        "url": "/functionalprogramming/2025/01/01/fpwelcome-2025.html",
+        "url": "/functionalprogramming/2025/05/01/fpwelcome-2025.html",
         "teaser": null
       }]
