bba5fa8e9731e2b75410777c087624bdeb91bd1f
Martin Steffen <msteffen@ifi.uio.no>
2025-08-22 14:50:23 +0200

    --

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Welcome to Functional programming (IN2040), autumn 2025",
-        "excerpt":"About this semester Lectures Corona is a thing of the dim pasts and lectures will be held the way it used to be, in the lecture hall (Simula@OJD). Youtube One perhaps positive effect of the virus times was that many lectures produced video-ed versions of the presentations, like screencasts or...","categories": ["functionalprogramming"],
+        "excerpt":"About this semester Lectures Corona is a thing of the dim pasts and lectures will be held the way it used to be, in the lecture hall (Simula@OJD). Youtube (resp. Panopto) One perhaps positive effect of the virus times was that many lectures produced video-ed versions of the presentations, like...","categories": ["functionalprogramming"],
         "tags": ["functional programming","IN2040","scheme","lisp"],
         "url": "/functionalprogramming/2025/05/01/fpwelcome-2025.html",
+        "teaser": null
+      },{
+        "title": "Evaluation strategies",
+        "excerpt":"The post is about evaluation strategies. The concept is discussed in the lecture (in week 2) and in SICP. Especially applicative order evaluation is covered, as the standard evaluation strategy of Scheme. Also, an alternative to that is discussed, namely normal order evaluation, and there is a connection with things...","categories": ["functionalprogramming"],
+        "tags": ["functional programming","IN2040","scheme","lisp","evaluation strategy","normal-order evaluation strategy","applicative-order evaluation strategy","referential transparency"],
+        "url": "/functionalprogramming/2025/08/22/evaluationstrategies.html",
         "teaser": null
       }]
