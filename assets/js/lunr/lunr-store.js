78659f6e140ece1eaf899bf244d47c0e7cef816c
Martin Steffen <msteffen@ifi.uio.no>
2025-09-18 06:03:18 +0200

    --

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -10,4 +10,22 @@ var store = [{
         "tags": ["functional programming","IN2040","scheme","lisp","evaluation strategy","normal-order evaluation strategy","applicative-order evaluation strategy","referential transparency"],
         "url": "/functionalprogramming/2025/08/22/evaluationstrategies.html",
         "teaser": null
+      },{
+        "title": "What's in a name?",
+        "excerpt":"In the break during the lecture in week 2, a question came up from the audience asking for more information and explanations in connection with some slide titled “Anecdote”. Discussed at (and around) that slide are formal parameters, free and bound variables, and the notion of scope. The slides state...","categories": ["functionalprogramming"],
+        "tags": ["functional programming","IN2040","scheme","lisp","variables","scope","variable capture","variable renaming","static scoping","dynamic scoping"],
+        "url": "/functionalprogramming/2025/09/01/bindingandscope.html",
+        "teaser": null
+      },{
+        "title": "Processes and procedures",
+        "excerpt":"Previous semester I got feedback that the concepts of processes vs procedures and functions remained shady and a bit unclear (at least for some). Thus I write a few lines on that. I won’t repeat the examples and sentences from SICP; there is Section 1.2 (“Procedures and the processes generate”)...","categories": ["functionalprogramming"],
+        "tags": ["procedure","process","function"],
+        "url": "/functionalprogramming/2025/09/05/processprocedure.html",
+        "teaser": null
+      },{
+        "title": "Recursion, primitive or otherwise",
+        "excerpt":"The SICP textbook shows in Exercise 1.10 a famous function known as Ackermann’s function. Actually the code there shows one version of that function; there are minor variations of it, all doing basically the same, and all known as Ackermann function. The exercise is not on the list of exercises...","categories": ["functionalprogramming"],
+        "tags": ["functional programming","IN2040","scheme","lisp","Ackermann","currying","higher-order functions","recursion","primitive recursion"],
+        "url": "/functionalprogramming/2025/09/12/ackermann.html",
+        "teaser": null
       }]
