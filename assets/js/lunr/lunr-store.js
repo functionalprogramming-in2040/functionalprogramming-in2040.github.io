1c5d7744883517646ae65dd445b60f9708e84aff
Martin Steffen <msteffen@ifi.uio.no>
2025-10-15 12:08:55 +0200

    --

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": null
       },{
         "title": "What's in a name?",
-        "excerpt":"In the break during the lecture in week 2, a question came up from the audience asking for more information and explanations in connection with some slide titled “Anecdote”. Discussed at (and around) that slide are formal parameters, free and bound variables, and the notion of scope. The slides state...","categories": ["functionalprogramming"],
+        "excerpt":"In the lecture in week 2 in 2024, a question came up from the audience asking for more information and explanations in connection with some slide titled “Anecdote”. Discussed at (and around) that slide are formal parameters, free and bound variables, and the notion of scope. The slides state that...","categories": ["functionalprogramming"],
         "tags": ["functional programming","IN2040","scheme","lisp","variables","scope","variable capture","variable renaming","static scoping","dynamic scoping"],
         "url": "/functionalprogramming/2025/09/01/bindingandscope.html",
         "teaser": null
@@ -27,5 +27,17 @@ var store = [{
         "excerpt":"The SICP textbook shows in Exercise 1.10 a famous function known as Ackermann’s function. Actually the code there shows one version of that function; there are minor variations of it, all doing basically the same, and all known as Ackermann function. The exercise is not on the list of exercises...","categories": ["functionalprogramming"],
         "tags": ["functional programming","IN2040","scheme","lisp","Ackermann","currying","higher-order functions","recursion","primitive recursion"],
         "url": "/functionalprogramming/2025/09/12/ackermann.html",
+        "teaser": null
+      },{
+        "title": "Y not code up $n!$ with no recursion and no Y tricks either?",
+        "excerpt":"In another post, I dissected how to program recursion using anonymous functions, only. This ultimately leads to the famous $Y$-combinator. Actually, there are multiple versions of it, all doing the recursion-trick by some form of self-application. The running example in that blog post was the inevitable factorial function. If that...","categories": ["functionalprogramming"],
+        "tags": ["Church numerals","lambda calculus","induction","for loops","recursion","foundations","pattern matching","inductive data types"],
+        "url": "/functionalprogramming/2025/10/15/churchnums.html",
+        "teaser": null
+      },{
+        "title": "Y Y?",
+        "excerpt":"This is another post in connection with some slides shown in the lecture, which may have been a bit obscure. The text here is concretely triggered by a slide in week 9 about “recursion with anonymous procedures”. The slide showed a version of the factorial function programmed in a way...","categories": ["functionalprogramming"],
+        "tags": ["Y combinator","lambda calculus","recursion","Turing completeness","computable functions","foundation","Church numerals"],
+        "url": "/functionalprogramming/2025/10/15/ycombinator.html",
         "teaser": null
       }]
